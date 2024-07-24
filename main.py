dd8637d4dc52c2ea49ef437f703ba53f1b5a258f
Aaron Limb <alimb1@students.avc.edu>
2024-07-23 23:09:11 -0700

    Lab 3 done

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ def main():
     num1 = int(input('Enter the first number: '))
     num2 = int(input('Enter the second number: '))
     num3 = int(input('Enter the third number: '))
-    """
-    Code Your Program here
-    """
 
-    print(f'The greates number is {maxnum}')
-    ########################################
-    # Do not delete the return statement
-    ########################################
+    if num1 >= num2 and num1 >= num3:
+        maxnum = num1
+    elif num2 >= num1 and num2 >= num3:
+        maxnum = num2
+    else:
+        maxnum = num3
+
+    print(f'The greatest number is {maxnum}')
+    
     return maxnum
 
 
